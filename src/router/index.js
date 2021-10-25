2bf1c4a866b1844df4ff2d2b9d273a68c12f7c96
alifakoor <ali.fakoor13@gmail.com>
2021-10-25 20:28:54 +0330

    add product section routes

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,34 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/username',
+    name: 'user.show',
+    component: () => {},
+    children: [
+      {
+        path: 'products',
+        name: 'porduct.show',
+        component: () => {},
+        children: [
+          {
+            path: 'create',
+            name: 'product.create',
+            component: () => {}
+          },
+          {
+            path: ':id/update',
+            name: 'product.update',
+            component: () => {}
+          },
+          {
+            path: ':id/delete',
+            name: 'product.delete'
+          }
+        ]
+      }
+    ]
   }
 ]
 
