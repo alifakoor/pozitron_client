1d0a07926d79646aca41aa78f9e67f3df9d29961
alifakoor <ali.fakoor13@gmail.com>
2021-10-30 21:20:28 +0330

    add primevue

--- src/main.js
+++ src/main.js
@@ -3,5 +3,19 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import PrimeVue from 'primevue/config'
+import components from './components'
 
-createApp(App).use(store).use(router).mount('#app')
+// css from the Primevue
+import 'primevue/resources/themes/saga-blue/theme.css' // theme
+import 'primevue/resources/primevue.min.css' // core css
+import 'primeicons/primeicons.css' // icons
+import 'primeflex/primeflex.css' // flex
+
+const app = createApp(App).use(store).use(router).use(PrimeVue)
+
+// use components
+components.install(app)
+
+// mount
+app.mount('#app')
