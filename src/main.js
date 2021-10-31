471b87cdb3a66bd1cbce06281ad1ca8aa5312f85
alifakoor <ali.fakoor13@gmail.com>
2021-10-31 15:27:45 +0330

    downgrade primeflex to version 2.0.0, because its version 3.1.0 not working well \ninstall remixicon

--- src/main.js
+++ src/main.js
@@ -12,6 +12,10 @@ import 'primevue/resources/primevue.min.css' // core css
 import 'primeicons/primeicons.css' // icons
 import 'primeflex/primeflex.css' // flex
 
+// remixicon
+import 'remixicon/fonts/remixicon.css'
+
+// create app
 const app = createApp(App).use(store).use(router).use(PrimeVue)
 
 // use components
